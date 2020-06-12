d19a8189ffef27366fc2c843bcc9a5967bef1805
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-06-12 19:51:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -80,13 +80,13 @@ class Dog
     
   end
   
-  def self.find_or_create_by(attributes)
+  def self.find_or_create_by(name:, breed:)
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
     if !dog.empty?
       dog_data = dog[0]
       new_dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
     else
-      new_dog = self.create(attributes)
+      new_dog = self.create(name, breed)
     end
     new_dog
   end
